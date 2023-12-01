220dc5e73badcd1ed251bd724bae2ce540a23ec6
DarynaSD <dasha.dermanchuk@gmail.com>
2023-12-01 21:28:51 +0200

    start 3 hw

---
+++ db/connect.js
@@ -0,0 +1,5 @@
+const mongoose = require("mongoose")
+
+const DB_HOST = "mongodb+srv://dashadermanchuk:ZP4wECGRfNwXWhne@contactscluster.77apus7.mongodb.net/contactsDB?retryWrites=true&w=majority"
+
+mongoose.connect()
