d383d379f6cb14b8e601e5541de2a0565f3f8efc
DarynaSD <dasha.dermanchuk@gmail.com>
2023-12-02 20:43:19 +0200

    move db_host to db_config.js

--- db/connect.js
+++
@@ -1,5 +0,0 @@
-const mongoose = require("mongoose")
-
-const DB_HOST = "mongodb+srv://dashadermanchuk:ZP4wECGRfNwXWhne@contactscluster.77apus7.mongodb.net/contactsDB?retryWrites=true&w=majority"
-
-mongoose.connect()
--- server.js
+++ server.js
@@ -1,5 +1,17 @@
-const app = require('./app')
+const app = require("./app");
+const mongoose = require("mongoose");
+const DB_HOST = require("./helpers/db_config")
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+mongoose.set("strictQuery", true);
+
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    console.log("DB connect successfull");
+    app.listen(3000);
+    console.log("Server running. Use our API on port: 3000");
+  })
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
