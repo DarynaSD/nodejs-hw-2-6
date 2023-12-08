85ccbe7cf0193adbb25d98e4f0a111dfb0142a60
DarynaSD <dasha.dermanchuk@gmail.com>
2023-12-08 18:45:45 +0200

    deleted config with password

--- helpers/db_config.js
+++ helpers/db_config.js
@@ -1,4 +1,4 @@
-const DB_HOST =
-    "mongodb+srv://dashadermanchuk:ZP4wECGRfNwXWhne@contactscluster.77apus7.mongodb.net/contactsDB?retryWrites=true&w=majority";
+// const DB_HOST =
+//     "mongodb+srv://dashadermanchuk:ZP4wECGRfNwXWhne@contactscluster.77apus7.mongodb.net/contactsDB?retryWrites=true&w=majority";
   
-    module.exports = DB_HOST
+//     module.exports = DB_HOST
